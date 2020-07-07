ff716307f2857eec4be6769118d18a21b05b1bac
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-07-07 12:22:31 +0100

    fixed default construct for perlin

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char** argv)
 
     GLuint texture, program, vertexBuffer;
     GLint mvpLocation, vposLocation, colorLocation, textureLocation;
-    Perlin perlin = Perlin(232u);
+    Perlin perlin = Perlin();
     // create OpenGL objects
     {
         char pixels[perlinSize * perlinSize];
